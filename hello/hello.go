5115877f52d6cd73f3bd27e540fb4439925e51db
Ben Gunn <bengunn.dev@gmail.com>
2023-08-29 14:05:46 +0630

    add return map

--- hello/hello.go
+++ hello/hello.go
@@ -12,11 +12,15 @@ func main() {
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
 
-	message, err := greetings.Hello("Ben")
+	names := []string{
+		"Ben", "Gunn", "Jeff",
+	}
+
+	messages, err := greetings.Hellos(names)
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println(message)
+	fmt.Println(messages)
 }
