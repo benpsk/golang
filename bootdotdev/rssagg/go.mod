32eace69cf0913ada732bb1c961b26b51d7c4e3b
Ben Gunn <bengunn.dev@gmail.com>
2023-11-27 23:44:07 +0630

    golang: add rssagg

--- bootdotdev/rssagg/go.mod
+++ bootdotdev/rssagg/go.mod
@@ -2,4 +2,8 @@ module github.com/benpsk/golang/bootdotdev/rssagg
 
 go 1.21.0
 
-require github.com/joho/godotenv v1.5.1
+require (
+	github.com/go-chi/chi v1.5.5
+	github.com/go-chi/cors v1.2.1
+	github.com/joho/godotenv v1.5.1
+)
---
+++ bootdotdev/rssagg/json.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+func respondWithError(w http.ResponseWriter, code int, msg string) {
+	if code > 499 {
+		log.Printf("Responding with 5xx error: %s", msg)
+	}
+	type errorResponse struct {
+		Error string `json:"error"`
+	}
+	respondWithJSON(w, code, errorResponse{
+		Error: msg,
+	})
+}
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	data, err := json.Marshal(payload)
+	if err != nil {
+		log.Printf("Error mashalling JSON: %s", err)
+		return
+	}
+	w.WriteHeader(code)
+	w.Write(data)
+}
