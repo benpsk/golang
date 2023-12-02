495aa9a4507f4d0a59053d55b262d969107b931f
Ben Gunn <bengunn.dev@gmail.com>
2023-12-02 20:28:21 +0630

    go: add postgresql

--- bootdotdev/rssagg/main.go
+++ bootdotdev/rssagg/main.go
@@ -1,20 +1,39 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
 	"os"
 
+	"github.com/benpsk/golang/bootdotdev/rssagg/internal/database"
 	"github.com/go-chi/chi"
 	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
+type apiConfig struct {
+	DB *database.Queries
+}
+
 func main() {
-	godotenv.Load()
-	portString := os.Getenv("PORT")
-	if portString == "" {
+	godotenv.Load(".env")
+	port := os.Getenv("PORT")
+	if port == "" {
 		log.Fatal("Port is not found in the environment")
+	}
+	dbUrl := os.Getenv("DB_URL")
+	if dbUrl == "" {
+		log.Fatal("Database URL env is not found in the environment")
+	}
+	db, err := sql.Open("postgres", dbUrl)
+	if err != nil {
+		log.Fatal(err)
+	}
+	dbQueries := database.New(db)
+	apiCfg := apiConfig{
+		DB: dbQueries,
 	}
 	router := chi.NewRouter()
 	router.Use(cors.Handler(cors.Options{
@@ -26,17 +45,14 @@ func main() {
 		MaxAge:           300,
 	}))
 	v1Router := chi.NewRouter()
+	v1Router.Post("/users", apiCfg.handleUsersCreate)
 	v1Router.Get("/healthz", handleReadiness)
 	v1Router.Get("/err", handleErr)
 	router.Mount("/v1", v1Router)
-
 	srv := &http.Server{
 		Handler: router,
-		Addr:    ":" + portString,
+		Addr:    ":" + port,
 	}
-	log.Printf("Server starting on port %v", portString)
-	err := srv.ListenAndServe()
-	if err != nil {
-		log.Fatal(err)
-	}
+	log.Printf("Server starting on port %v", port)
+	log.Fatal(srv.ListenAndServe())
 }
