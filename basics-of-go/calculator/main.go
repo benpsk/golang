f127b07915e2afa68b118697d2d1a1c8ea649cf3
Ben Gunn <bengunn.dev@gmail.com>
2024-03-01 08:44:22 +0630

    fem: calculator start

---
+++ basics-of-go/calculator/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var operation string
+	var number1, number2 int
+
+  fmt.Println('CALCULATOR GO 1.0')
+  fmt.Println("-----------------")
+  fmt.Println("Which operation do you want to perform? (add, substract, multiply, divided)")
+  fmt.Scanf("%s")
+}
