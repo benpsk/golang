3f1e445c145a2abe7a86eb9f9cf937cabddcec7c
Ben Gunn <bengunn.dev@gmail.com>
2024-03-03 11:23:45 +0630

    doc: how to write go code

---
+++ hello2/hello2.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"example/benpsk/hello2/morestrings"
+	"fmt"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+func main() {
+	fmt.Println(morestrings.ReverseRunes("!oG ,olleH"))
+	fmt.Println(cmp.Diff("Hello World", "Hello Go"))
+}
