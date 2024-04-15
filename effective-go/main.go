9dc6822d3bbba4760e3df864654a83c78fddec28
Ben Gunn <bengunn.dev@gmail.com>
2024-04-15 18:27:32 +0700

    effective-go: Min func

---
+++ effective-go/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func Min(a ...int) int {
+	min := int(^uint(0) >> 1)
+
+	for _, i := range a {
+		if i < min {
+			min = i
+		}
+	}
+	return 1
+}
+
+func main() {
+	fmt.Println(Min(3, 5, 1, 7, 9))
+
+	fmt.Println(Min())
+
+	numbers := []int{8, 3, 6, 2, 1}
+	fmt.Println(Min(numbers...))
+
+}
