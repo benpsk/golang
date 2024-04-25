4e6a09618d1ae0325b4819aaf03316a7a7766e63
Ben Gunn <bengunn.dev@gmail.com>
2024-04-25 20:20:40 +0700

    gin-auth: bearer token

--- gin/authentication/auth.go
+++ gin/authentication/auth.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"crypto/rand"
+	"encoding/hex"
+	"fmt"
 	"log"
 	"net/http"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
+
+var tokens []string
 
 func main() {
 	r := gin.Default()
@@ -16,5 +22,41 @@ func main() {
 			"data": "basic auth success",
 		})
 	})
+
+	r.POST("login", gin.BasicAuth(gin.Accounts{
+		"admin": "secret",
+	}), func(c *gin.Context) {
+		token, _ := randomHex(20)
+		tokens = append(tokens, token)
+		c.JSON(http.StatusOK, gin.H{
+			"token": token,
+		})
+	})
+
+	r.GET("bearer-auth", func(c *gin.Context) {
+		bearerToken := c.Request.Header.Get("Authorization")
+		reqToken := strings.Split(bearerToken, " ")[1]
+		fmt.Println("tokens => ", tokens)
+		fmt.Println("reqToken => ", reqToken)
+		for _, token := range tokens {
+			if token == reqToken {
+				c.JSON(http.StatusOK, gin.H{
+					"data": "bearer auth token success",
+				})
+				return
+			}
+		}
+		c.JSON(http.StatusUnauthorized, gin.H{
+			"message": "unauthorized",
+		})
+	})
 	log.Fatal(r.Run()) // default port :8080
+}
+
+func randomHex(n int) (string, error) {
+	bytes := make([]byte, n)
+	if _, err := rand.Read(bytes); err != nil {
+		return "", err
+	}
+	return hex.EncodeToString(bytes), nil
 }
