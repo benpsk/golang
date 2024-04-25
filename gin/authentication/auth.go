8027cbd6cf10f3954c01de6dca27a8fc5d84bd91
Ben Gunn <bengunn.dev@gmail.com>
2024-04-26 00:14:44 +0700

    gin-auth: jwt token

--- gin/authentication/auth.go
+++ gin/authentication/auth.go
@@ -7,11 +7,19 @@ import (
 	"log"
 	"net/http"
 	"strings"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
 )
 
+var jwtKey = []byte("my_secret_key")
 var tokens []string
+
+type Claims struct {
+	Username string `json:"username"`
+	jwt.RegisteredClaims
+}
 
 func main() {
 	r := gin.Default()
@@ -23,7 +31,7 @@ func main() {
 		})
 	})
 
-	r.POST("login", gin.BasicAuth(gin.Accounts{
+	r.POST("bearer-login", gin.BasicAuth(gin.Accounts{
 		"admin": "secret",
 	}), func(c *gin.Context) {
 		token, _ := randomHex(20)
@@ -50,6 +58,47 @@ func main() {
 			"message": "unauthorized",
 		})
 	})
+
+	r.POST("jwt-login", gin.BasicAuth(gin.Accounts{
+		"admin": "secret",
+	}), func(c *gin.Context) {
+		token, _ := generateJWT()
+		tokens = append(tokens, token)
+
+		c.JSON(http.StatusOK, gin.H{
+			"token": token,
+		})
+	})
+
+	r.GET("/jwt-auth", func(c *gin.Context) {
+		bearerToken := c.Request.Header.Get("Authorization")
+		reqToken := strings.Split(bearerToken, " ")[1]
+		claims := &Claims{}
+		tkn, err := jwt.ParseWithClaims(reqToken, claims, func(token *jwt.Token) (interface{}, error) {
+			return jwtKey, nil
+		})
+		if err != nil {
+			if err == jwt.ErrSignatureInvalid {
+				c.JSON(http.StatusUnauthorized, gin.H{
+					"message": "unauthorized",
+				})
+				return
+			}
+			c.JSON(http.StatusBadRequest, gin.H{
+				"message": "bad request",
+			})
+			return
+		}
+		if !tkn.Valid {
+			c.JSON(http.StatusUnauthorized, gin.H{
+				"message": "unauthorized",
+			})
+			return
+		}
+		c.JSON(http.StatusOK, gin.H{
+			"data": "jwt auth successful.",
+		})
+	})
 	log.Fatal(r.Run()) // default port :8080
 }
 
@@ -59,4 +108,16 @@ func randomHex(n int) (string, error) {
 		return "", err
 	}
 	return hex.EncodeToString(bytes), nil
+}
+
+func generateJWT() (string, error) {
+	expirationTime := time.Now().Add(5 * time.Minute)
+	claims := &Claims{
+		Username: "username",
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(expirationTime),
+		},
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString(jwtKey)
 }
